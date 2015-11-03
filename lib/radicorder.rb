92d6c2b8c2f4425487c1f4a5ab1a50c4230fa9ef
cnosuke <shinnosuke@gmail.com>
2015-11-04 00:13:55 +0900

    hotfix

--- lib/radicorder.rb
+++ lib/radicorder.rb
@@ -8,12 +8,12 @@ unless RUBY_VERSION > '2.1'
 end
 
 module Radicorder
-  PLAYER_URL = 'http://radiko.jp/player/swf/player_3.0.0.01.swf'
-  SWFEXTRACT_PATH = '/usr/bin/swfextract'
-  AUTH1_FMS_URL = 'https://radiko.jp/v2/api/auth1_fms'
-  AUTH2_FMS_URL = 'https://radiko.jp/v2/api/auth2_fms'
-  RTMPDUMP_PATH = '/usr/bin/rtmpdump'
-  RTMP_STREAM_URL = 'rtmpe://w-radiko.smartstream.ne.jp'
-  AVCONV_PATH = '/usr/bin/avconv'
-  AVCONV_OPT = '-acodec libmp3lame -ab 64k -ac 1 -ar 44100'
+  PLAYER_URL = ENV['PLAYER_URL'] || 'http://radiko.jp/player/swf/player_3.0.0.01.swf'
+  SWFEXTRACT_PATH = ENV['SWFEXTRACT_PATH'] || '/usr/bin/swfextract'
+  AUTH1_FMS_URL = ENV['AUTH1_FMS_URL'] || 'https://radiko.jp/v2/api/auth1_fms'
+  AUTH2_FMS_URL = ENV['AUTH2_FMS_URL'] || 'https://radiko.jp/v2/api/auth2_fms'
+  RTMPDUMP_PATH = ENV['RTMPDUMP_PATH'] || '/usr/bin/rtmpdump'
+  RTMP_STREAM_URL = ENV['RTMP_STREAM_URL'] || 'rtmpe://f-radiko.smartstream.ne.jp'
+  AVCONV_PATH = ENV['AVCONV_PATH'] || '/usr/bin/avconv'
+  AVCONV_OPT = ENV['AVCONV_OPT'] || '-acodec libmp3lame -ab 64k -ac 1 -ar 44100'
 end
